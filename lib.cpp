a85afed2931ecc5e8a99c5ce6d674cd1783b54d8
s5433430 <115527969+s5433430@users.noreply.github.com>
2022-12-12 18:18:34 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,9 @@
-
+float maggiore (float numeri [], int a) {
+for (int i=0; i<a; ++i) {
+    if (numeri [0] > numeri [i]){
+    }else {
+        numeri [0] = numeri [i];
+    }
+}
+return numeri [0];
+}

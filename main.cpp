7ce0965539c7f5a2a2f7dea460081d888450a478
s5433430 <115527969+s5433430@users.noreply.github.com>
2022-12-12 18:19:11 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
-
-int main(){
-  return 0;
+#include"lib.h"
+using namespace std;
+int main() {
+    int a = 0;
+    cin >> a;
+    float numeri [a];
+    for (int i = 0; i < a; i++ ){
+        cin >> numeri [i];
+    }
+   cout << maggiore ( numeri, a) <<endl;
+    return 0;
 }
